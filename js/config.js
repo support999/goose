c6ae4203ea00f3d89da098659a1f7b145ec37696
Amit Saxena <aamitsaxenaa1@gmail.com>
2022-05-27 17:34:58 +0530

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,24 @@
+                                               
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["We are into Mobile Application", "We are in Datawarehouse & Analytics", "We are into Qlik Sence | Qlik View"];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Front page effect - can be 'none', 'particles', 'particle-fountain', 'video'  */
+var config_frontPageEffect = 'video';
+
+/* If config_frontPageEffect is set to video, here set the video URL, type and opacity */
+var config_videoUrl = 'video/background.mp4';
+var config_videoType = 'video/mp4';
+var config_videoOpacity = 0.5;
+
+/* Google map parameters */
+var config_mapLat = 51.509036; // latitude
+var config_mapLng = -0.123280; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My company' // marker title
